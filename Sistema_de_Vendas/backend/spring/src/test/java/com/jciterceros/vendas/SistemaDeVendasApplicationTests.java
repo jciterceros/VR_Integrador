64637de09763b0c6f18fa3e294155de7ddf1ee0d
jciterceros <jciterceros@yahoo.com>
2024-06-16 18:44:06 -0300

    add initial BackEnd Project using Spring

---
+++ Sistema_de_Vendas/backend/spring/src/test/java/com/jciterceros/vendas/SistemaDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jciterceros.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
